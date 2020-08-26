33a3ede1513d3d085f9cdd412c5d40eb3c3ad9ea
JeongWonYoung <57868072+reane0809@users.noreply.github.com>
2020-08-26 13:38:54 +0900

    Update test_3.sql

--- test_3.sql
+++ test_3.sql
@@ -7,7 +7,7 @@ CREATE USER 'student'@'%' IDENTIFIED BY 'studentpassword' ;
 grant select on 학사DB.course TO 'student'@'%';
 grant select on 학사DB.student TO 'student'@'%'; 
 grant select on 학사DB.course_taken TO 'student'@'%';
-grant select on 학사DB.department TO 'student'@'%'; 
+grant select on 학사DB.department TO 'student'@'%';
 grant select on 학사DB.instructor TO 'student'@'%';
  
 show grants for student;
