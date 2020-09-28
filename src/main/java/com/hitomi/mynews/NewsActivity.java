7699284c14446fc5f4c19ace2616e3925833e1ba
JeongWonYoung <57868072+reane0809@users.noreply.github.com>
2020-09-28 11:07:51 +0900

    Update NewsActivity.java

--- src/main/java/com/hitomi/mynews/NewsActivity.java
+++ src/main/java/com/hitomi/mynews/NewsActivity.java
@@ -10,7 +10,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView; 
 
 import com.android.volley.Request;
-import com.android.volley.RequestQueue;
+import com.android.volley.RequestQueue; 
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
