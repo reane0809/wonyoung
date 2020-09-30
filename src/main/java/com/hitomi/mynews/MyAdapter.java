1f50b0c63852ebd44bffc85458b736c948a269e4
JeongWonYoung <57868072+reane0809@users.noreply.github.com>
2020-09-30 10:49:02 +0900

    Update MyAdapter.java

--- src/main/java/com/hitomi/mynews/MyAdapter.java
+++ src/main/java/com/hitomi/mynews/MyAdapter.java
@@ -6,32 +6,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView; 
-
+ 
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
  
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.facebook.drawee.view.SimpleDraweeView;
--
-import com.facebook.drawee.view.SimpleDraweeView;
-
-import java.util.List;
-
-public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
- private List<NewsData> mDataset;
- 
- public static class MyViewHolder extends RecyclerView.ViewHolder {
-  public TextView TextView_title;
-  public TextView TextView_content;
-  public ImageView ImageView_news;
-  
-  public MyViewHolder(View v) {
-   super(v);
-   TextView_title = v.findViewById(R.id.TextView_title);
-   TextView_content = v.findViewById(R.id.TextView_content);
-   ImageView_news = v.findViewById(R.id.ImageView_news);
-   -
 
 import java.util.List;
 
