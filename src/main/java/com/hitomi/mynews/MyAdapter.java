49b35a40fbbccf8e38aca7d2543203171c1aec78
JeongWonYoung <57868072+reane0809@users.noreply.github.com>
2020-11-11 00:16:00 +0900

    Update MyAdapter.java

--- src/main/java/com/hitomi/mynews/MyAdapter.java
+++ src/main/java/com/hitomi/mynews/MyAdapter.java
@@ -9,7 +9,7 @@ import android.widget.ImageView;
  
 import android.widget.TextView;
 
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView; 
  
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.facebook.drawee.view.SimpleDraweeView;
