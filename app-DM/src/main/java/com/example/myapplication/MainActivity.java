2764d0adfcd3adcc921a7db989ae9858d693ae75
JeongWonYoung <57868072+reane0809@users.noreply.github.com>
2020-11-23 01:42:11 +0900

    Update MainActivity.java

--- app-DM/src/main/java/com/example/myapplication/MainActivity.java
+++ app-DM/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity; 
-import android.os.Bundle; 
+import android.os.Bundle;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView; 
