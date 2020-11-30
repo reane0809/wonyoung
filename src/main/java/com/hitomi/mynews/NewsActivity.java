bb46295f59c58562a24294c61c9d876e809e5ffe
JeongWonYoung <57868072+reane0809@users.noreply.github.com>
2020-11-30 12:45:47 +0900

    Update NewsActivity.java

--- src/main/java/com/hitomi/mynews/NewsActivity.java
+++ src/main/java/com/hitomi/mynews/NewsActivity.java
@@ -2,7 +2,7 @@ package com.hitomi.mynews;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
+import android.view.View; 
 
 import androidx.appcompat.app.AppCompatActivity;
 
